e30661c9426e521b2e81298c965a3996fc3936f7
koraish3438 <koraishmdkajamall38@gmail.com>
2025-10-18 23:32:02 +0600

    Splash,Validation, Create Account and Login page complete

--- app/src/main/java/com/example/bmicalculator/ActivityCreateAccount.kt
+++ app/src/main/java/com/example/bmicalculator/ActivityCreateAccount.kt
@@ -7,7 +7,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bmicalculator.databinding.ActivityCreateAccountBinding
 
-
 class ActivityCreateAccount : AppCompatActivity() {
     private lateinit var binding: ActivityCreateAccountBinding
 
@@ -16,15 +15,16 @@ class ActivityCreateAccount : AppCompatActivity() {
         binding = ActivityCreateAccountBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.toolbar.setNavigationOnClickListener {
-            onBackPressedDispatcher.onBackPressed()
+        binding.back.setOnClickListener {
+            val intent = Intent(this, ActivityLogin::class.java)
+            startActivity(intent)
         }
 
         binding.regBtn.setOnClickListener {
-            val name = binding.nameInput.text.toString().trim()
-            val email = binding.emailInput.text.toString().trim()
-            val password = binding.passwordInput.text.toString().trim()
-            val confirmPassword = binding.confirmPasswordInput.text.toString().trim()
+            val name = binding.etName.text.toString().trim()
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            val confirmPassword = binding.etConfirmPassword.text.toString().trim()
 
             if (name.isEmpty()) {
                 Toast.makeText(this, "Name required", Toast.LENGTH_SHORT).show()
@@ -40,16 +40,25 @@ class ActivityCreateAccount : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            if (password.length < 8) {
+            if (password.isEmpty()) {
+                Toast.makeText(this, "Password required", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            else if (password.length < 8) {
                 Toast.makeText(this, "Password must be at least 8 characters", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            if (confirmPassword != password) {
+            if (confirmPassword.isEmpty()) {
+                Toast.makeText(this, "Confirm password required", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            else if (confirmPassword != password) {
                 Toast.makeText(this, "Password not match", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
+            Toast.makeText(this, "Account created successfully", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, ActivityBMI::class.java)
             startActivity(intent)
 
--- app/src/main/java/com/example/bmicalculator/ActivityLogin.kt
+++ app/src/main/java/com/example/bmicalculator/ActivityLogin.kt
@@ -1,20 +1,53 @@
 package com.example.bmicalculator
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.bmicalculator.databinding.ActivityLoginBinding
 
 class ActivityLogin : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backLogin.setOnClickListener {
+            val intent = Intent(this, ActivityCreateAccount::class.java)
+            startActivity(intent)
+        }
+        
+        binding.btnLoginNow.setOnClickListener {
+            val email = binding.etLoginEmail.text.toString().trim()
+            val password = binding.etLoginPassword.text.toString().trim()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Email required", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(this, "Invalid email address", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                Toast.makeText(this, "Password required", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            else if (password.length < 8) {
+                Toast.makeText(this, "Password must be at least 8 characters", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ActivityBMI::class.java)
+            startActivity(intent)
+        }
+        binding.goToRegister.setOnClickListener {
+            val intent = Intent(this, ActivityCreateAccount::class.java)
+            startActivity(intent)
         }
     }
 }
